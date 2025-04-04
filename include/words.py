d2b7d15d559627ef8e71ca5243697a089836301f
Aayush Bhandari <bhandariaayush074@gmail.com>
2025-04-04 19:16:36 +0545

    added files

---
+++ include/words.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+global rouge, vert, bleu, noir, jaune, orange, blanc, words
+
+words = [
+       ["Start", "Play"],  # 0
+       ["High Scores", "High Scores"],  # 1
+       ["Speed", "Speed"],  # 2
+       ["Special Thanks", "Special Thanks"],  # 3
+       ["Language", "Language"],  # 4
+       ["Quit", "Exit"],  # 5
+       ["Level: ", "Level: "],  # 6
+       ["Life: ", "Life: "],  # 7
+       ["Bomb: ", "Bomb: "],  # 8
+       ["Power: ", "Power: "],  # 9
+       ["Score: ", "Score: "],  # 10
+       ["Shield: ", "Shield: "],  # 11
+       ["<-- Left and Right -->", "<-- Left and Right -->"],  # 12
+       ["SpaceHack, it's stronger than you!", "SpaceHack, it's stronger than you!"],  # 13
+       ["Enter Your Name: ", "Enter Your Name: "],  # 14
+       ["Special Thanks to", "Special Thanks to"],  # 15
+       ["", ""],
+       ["Guido van Rossum", "Guido van Rossum"],  # 17
+       ["for the invention of this awesome language", "for the invention of this awesome language"],  # 18
+       ["", ""],
+       ["Aayush Bhandari and TEAM", "Aayush Bhandari and TEAM"],  # 20
+       ["for the splendid library and many more things...", "for the splendid library and many more things..."],  # 21
+       ["Visit http://www.pygame.org", "Visit http://www.pygame.org"],  # 22
+       ["", ""],
+       ["http://www.universal-soundbank.com/", "http://www.universal-soundbank.com/"],  # 24
+       ["for the sounds", "for the sounds"],  # 25
+       ["", ""],
+       ["Josmiley", "Josmiley"],  # 27
+       ["for the pretty library slidemenu", "for the pretty library slidemenu"],  # 28
+       ["", ""],  # 29
+       ["", ""],
+       ["Maya and Gabriel", "Maya and Gabriel"],  # 31
+       ["for their pretty drawings and good ideas", "for their pretty drawings and good ideas"],  # 32
+       ["", ""],
+       ["All guys for the precious feedback", "All guys for the precious feedback"],  # 34
+       ["", ""],
+       ["", ""],
+       ["", ""],
+       ["", ""],
+       ["", ""],
+       ["", ""],
+       ["SpaceMax", "SpaceMax"],  # 41
+       ["https://sites.google.com/site/gamemaxpy/", "https://sites.google.com/site/gamemaxpy/"],  # 42
+       ["©Copyleft 2025", "©Copyleft 2025"],  # 43
+       ["Scrolling", "Scrolling"],  # 44
+       ["Spatial Background", "Spatial Background"],  # 45
+       ["Random Polygons", "Random Polygons"],  # 46
+       ["Made By Aayush Bhandari", "Made By Aayush Bhandari"],  # 47
+]
